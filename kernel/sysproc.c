1088a338341b11a014972480e3dbe28d9b4b202b
lzlresume <2695919812@qq.com>
2024-05-05 08:07:06 +0000

    5 ok

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,17 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  // lazy allocation
+   if(n > 0) {
+    // lazy allocation
+    myproc()->sz += n;
+  } else if(myproc()->sz + n > 0) {
+    myproc()->sz = uvmdealloc(myproc()->pagetable, myproc()->sz, myproc()->sz + n);
+  } else {
     return -1;
+  }
+  // if(growproc(n) < 0)
+  //   return -1;
   return addr;
 }
 
